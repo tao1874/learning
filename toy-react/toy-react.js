c1290b08928e6298e118de3b857bd7b15f16a06b
michaelccao <michaelccao@tencent.com>
2020-08-25 23:53:48 +0800

    run react tutoria example

--- toy-react/toy-react.js
+++ toy-react/toy-react.js
@@ -1,18 +1,39 @@
+const RENDER_TO_DOM = Symbol('render to dom')
 class ElementWrapper {
     constructor(type) {
         this.root = document.createElement(type);
     }
     setAttribute(name, value) {
-        this.root.setAttribute(name, value);
+        if (name.match(/^on([\s\S]+)$/)) {
+            this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value)
+        }else {
+            if(name === 'className'){
+                this.root.setAttribute('class', value);
+            }
+            this.root.setAttribute(name, value);
+        }
+        
     }
     appendChild(component) {
-        this.root.appendChild(component.root);
+        let range = document.createRange()
+        range.setStart(this.root, this.root.childNodes.length)
+        range.setEnd(this.root, this.root.childNodes.length)
+        range.deleteContents()
+        component[RENDER_TO_DOM](range)
+    }
+    [RENDER_TO_DOM](range) {
+        range.deleteContents()
+        range.insertNode(this.root)
     }
 }
 
 class TextWrapper {
     constructor(content) {
         this.root = document.createTextNode(content);
+    }
+    [RENDER_TO_DOM](range) {
+        range.deleteContents()
+        range.insertNode(this.root)
     }
 }
 
@@ -23,13 +44,16 @@ export function createElement(type, attributes, ...children) {
     } else {
         e = new type
     }
-    for (let i in attributes) {
-        e.setAttribute(i, attributes[i]);
+    for (let name in attributes) {
+        e.setAttribute(name, attributes[name]);
     }
     let insetChildren = (children) => {
         for (let child of children) {
             if (typeof child === 'string') {
                 child = new TextWrapper(child);
+            }
+            if( child === null) {
+                continue
             }
             if (typeof child === 'object' && (child instanceof Array)) {
                 insetChildren(child);
@@ -43,7 +67,11 @@ export function createElement(type, attributes, ...children) {
 }
 
 export function render(component, parentElement) {
-    parentElement.appendChild(component.root);
+    let range = document.createRange()
+    range.setStart(parentElement, 0)
+    range.setEnd(parentElement, parentElement.childNodes.length)
+    range.deleteContents()
+    component[RENDER_TO_DOM](range)
 }
 
 export class Component {
@@ -51,6 +79,7 @@ export class Component {
         this.props = Object.create(null);
         this.children = [];
         this._root = null;
+        this._range = null
     }
     setAttribute(name, value) {
         this.props[name] = value;
@@ -58,11 +87,32 @@ export class Component {
     appendChild(component) {
         this.children.push(component);
     }
-
-    get root() {
-        if (!this._root) {
-            this._root = this.render().root;
+    [RENDER_TO_DOM](range) {
+        this._range = range
+        this.render()[RENDER_TO_DOM](range)
+    }
+    rerender() {
+        this._range.deleteContents()
+        this[RENDER_TO_DOM](this._range)
+    }
+    setState(newState) {
+        if (this.state === null || typeof this.state !== 'object') {
+            this.state = newState
+            this.rerender()
+            return
         }
-        return this._root;
+        const merge = (oldState, newState) => {
+            
+            for (const key in newState) {
+                if (oldState[key] !== null || typeof oldState[key] !== 'object') {
+                    oldState[key] = newState[key]
+                } else {
+                    merge(oldState[key], newState[key])
+                }
+            }
+
+        }
+        merge(this.state, newState)
+        this.rerender()
     }
 }
